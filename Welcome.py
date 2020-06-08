b63372a3f8646631573a9ea97976eac38774eedc
AnshulRastogi20 <66506553+AnshulRastogi20@users.noreply.github.com>
2020-06-08 08:50:09 +0530

    Add files via upload

---
+++ Welcome.py
@@ -0,0 +1,21 @@
+name , age = input("Enter your name and age").split(',')
+print("Welcome" + " " + name)
+age = int(age)
+if(age == 13):
+ print("You are in teenage")
+if(age == 14):
+ print("You are in teenage")
+if(age==15):
+ print("You are in teenage")
+if(age==16):
+ print("You are in teenage")
+if(age == 17):
+ print("You are in teenage")
+if(age==18):
+ print("You are in teenage")
+if(age==19):
+ print("You are in teenage")
+if(age>19):
+ print("You are in adulthood")
+if(age<13):
+ print("You are in childhood")
