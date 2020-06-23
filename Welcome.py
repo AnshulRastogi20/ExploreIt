01a57af42a9012f8bc695109f5f1015d3c30fae2
AnshulRastogi20 <66506553+AnshulRastogi20@users.noreply.github.com>
2020-06-23 20:33:41 +0530

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,20 +1,8 @@
 #This is a programme which wil welcom you when you will tell him your name and will also identify your category of age.
-name , age = input("Enter your name and age").split(',')
+name , age = input("Enter your name and age").split('')
 print("Welcome" + " " + name)
 age = int(age)
-if(age == 13):
- print("You are in teenage")
-if(age == 14):
- print("You are in teenage")
-if(age==15):
- print("You are in teenage")
-if(age==16):
- print("You are in teenage")
-if(age == 17):
- print("You are in teenage")
-if(age==18):
- print("You are in teenage")
-if(age==19):
+if(age >= 13 & age <=19):
  print("You are in teenage")
 if(age>19):
  print("You are in adulthood")
